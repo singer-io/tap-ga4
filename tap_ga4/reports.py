1ee64c4aa9cb123187d36fcdeb32ccaf9fefc25a
Leslie VanDeMark <lvandemark@talend.com>
2022-09-28 17:34:53 +0000

    add premade reports to the catalog

---
+++ tap_ga4/reports.py
@@ -0,0 +1,36 @@
+PREMADE_REPORTS = [
+    {
+        "name": "First User Medium Report",
+        "metrics": [
+            "newUsers",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "conversions",
+            "totalRevenue",
+            "totalUsers",
+            "userEngagementDuration"
+        ],
+        "dimensions": [
+            "date",
+            "firstUserMedium",
+        ],
+    },
+    {
+        "name": "First User Source Report",
+        "metrics": [
+            "newUsers",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "conversions",
+            "totalRevenue",
+            "totalUsers",
+            "userEngagementDuration"
+        ],
+        "dimensions": [
+            "date",
+            "firstUserSource",
+        ],
+    },
+]
