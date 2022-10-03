c734edd1b3867ddfd4948dc96f790f06972ffdc6
dsprayberry <28106103+dsprayberry@users.noreply.github.com>
2022-10-03 19:21:09 +0000

    Add to Premade Reports List

--- tap_ga4/reports.py
+++ tap_ga4/reports.py
@@ -9,7 +9,7 @@ PREMADE_REPORTS = [
             "conversions",
             "totalRevenue",
             "totalUsers",
-            "userEngagementDuration"
+            "userEngagementDuration",
         ],
         "dimensions": [
             "date",
@@ -26,11 +26,476 @@ PREMADE_REPORTS = [
             "conversions",
             "totalRevenue",
             "totalUsers",
-            "userEngagementDuration"
+            "userEngagementDuration",
         ],
         "dimensions": [
             "date",
             "firstUserSource",
         ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Name Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemName",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Id Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemId",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category Combined Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory1",
+            "itemCategory2",
+            "itemCategory3",
+            "itemCategory4",
+            "itemCategory5",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category 1 Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory1",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category 2 Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory2",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category 3 Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory3",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category 4 Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory4",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Category 5 Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+            "date",
+            "itemCategory5",
+        ],
+    },
+    {
+        "name": "Ecommerce Purchases Item Brand Report",
+        "metrics": [
+            "addToCarts",
+            "cartToViewRate",
+            "ecommercePurchases",
+            "itemPurchaseQuantity",
+            "itemRevenue",
+            "itemViews",
+            "purchaseToViewRate",
+        ],
+        "dimensions": [
+
+        ],
+    },
+    {
+        "name": "Publisher Ads Ad Unit Report",
+        "metrics": [
+            "adUnitExposure",
+            "publisherAdClicks",
+            "publisherAdImpressions",
+            "totalAdRevenue",
+        ],
+        "dimensions": [
+            "date",
+            "adUnitName",
+        ],
+    },
+    {
+        "name": "Publisher Ads Page Path Report",
+        "metrics": [
+            "adUnitExposure",
+            "publisherAdClicks",
+            "publisherAdImpressions",
+            "totalAdRevenue",
+        ],
+        "dimensions": [
+            "date",
+            "pagePath",
+        ],
+    },
+    {
+        "name": "Publisher Ads Ad Format Report",
+        "metrics": [
+            "adUnitExposure",
+            "publisherAdClicks",
+            "publisherAdImpressions",
+            "totalAdRevenue",
+        ],
+        "dimensions": [
+            "date",
+            "adFormat",
+        ],
+    },
+    {
+        "name": "Publisher Ads Ad Source Report",
+        "metrics": [
+            "adUnitExposure",
+            "publisherAdClicks",
+            "publisherAdImpressions",
+            "totalAdRevenue",
+        ],
+        "dimensions": [
+            "date",
+            "adSourceName",
+        ],
+    },
+    {
+        "name": "Demographic Country Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "country",
+        ],
+    },
+    {
+        "name": "Demographic Region Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "region",
+        ],
+    },
+    {
+        "name": "Demographic City Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "city",
+        ],
+    },
+    {
+        "name": "Demographic Language Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "language",
+        ],
+    },
+    {
+        "name": "Demographic Age Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "userAgeBracket",
+        ],
+    },
+    {
+        "name": "Demographic Gender Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "userGender",
+        ],
+    },
+    {
+        "name": "Demographic Interests Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "brandingInterest",
+        ],
+    },
+    {
+        "name": "Tech Browser Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "browser",
+        ],
+    },
+    {
+        "name": "Tech Device Category Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "deviceCategory",
+        ],
+    },
+    {
+        "name": "Tech Device Model Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "deviceModel",
+        ],
+    },
+    {
+        "name": "Tech Screen Resolution Report",
+        "metrics": [
+            "date",
+            "screenResolution",
+        ],
+        "dimensions": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+    },
+    {
+        "name": "Tech App Version Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "appVersion",
+        ],
+    },
+    {
+        "name": "Tech Platform Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "platform",
+        ],
+    },
+        {
+        "name": "Tech OS Version Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "operatingSystemVersion",
+        ],
+    },
+    {
+        "name": "Tech Platform and Device Category Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "deviceCatetory",
+            "platform",
+        ],
+    },
+    {
+        "name": "Tech Operating System Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount",
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+        ],
+        "dimensions": [
+            "date",
+            "operatingSystem"
+        ],
+    },
+    {
+        "name": "Tech OS with Version Report",
+        "metrics": [
+            "conversions",
+            "engagedSessions",
+            "engagementRate",
+            "eventCount"
+            "newUsers",
+            "totalRevenue",
+            "totalUsers",
+            ],
+        "dimensions": [
+            "date",
+            "operatingSystemWithVersion",
+            ],
     },
 ]
