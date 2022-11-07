96afc32762bc964094fa25953e2609d5fc69f71c
Leslie VanDeMark <lvandemark@talend.com>
2022-11-07 21:45:17 +0000

    Update bookmark test to use new methods

--- tests/test_bookmark.py
+++ tests/test_bookmark.py
@@ -70,17 +70,18 @@ class GA4BookmarkTest(BookmarkTest, GA4Base):
                 # gather results
                 stream_bookmark_1 = self.bookmarks_1.get(stream)
                 stream_bookmark_2 = self.bookmarks_2.get(stream)
-                bookmark_value_1 = self.get_bookmark_value(stream_bookmark_1, stream)
-                bookmark_value_2 = self.get_bookmark_value(stream_bookmark_2, stream)
+
+                bookmark_value_1 = self.get_bookmark_value(self.state_1, stream)
+                bookmark_value_2 = self.get_bookmark_value(self.state_2, stream)
 
                 # Verify the bookmark is set based on sync end date (today) for sync 1
                 # (The tap replicaates from the start date through to today)
-                parsed_bookmark_value_1 = dt.strptime(bookmark_value_1, self.BOOKMARK_FORMAT)
+                parsed_bookmark_value_1 = self.parse_date(bookmark_value_1)
                 self.assertEqual(parsed_bookmark_value_1, today_datetime)
 
                 # Verify the bookmark is set based on sync execution time for sync 2
                 # (The tap replicaates from the manipulated state through to todayf)
-                parsed_bookmark_value_2 = dt.strptime(bookmark_value_2, self.BOOKMARK_FORMAT)
+                parsed_bookmark_value_2 = self.parse_date(bookmark_value_2)
                 self.assertEqual(parsed_bookmark_value_2, today_datetime)
 
 
