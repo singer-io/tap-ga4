ab6f3cbb80699c513d6e97ef01e005188829a275
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2022-12-06 15:09:11 -0500

    V0.0.14 (#34)
    * update field exclusions after data api change
    
    * version bump to 0.0.14 and changelog update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-ga4",
-    version="0.0.13",
+    version="0.0.14",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
