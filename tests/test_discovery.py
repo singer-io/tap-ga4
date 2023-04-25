1f7ece1bc33e9a559c57caef6e4a10df4a84183f
Harrison Rose <harrison@stitchdata.com>
2023-04-25 17:31:26 -0400

    unskip test_stream_naming and override streams to test for a single test

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,5 +1,3 @@
-import unittest
-
 from tap_tester.base_suite_tests.discovery_test import DiscoveryTest
 
 from base import GA4Base
@@ -15,8 +13,17 @@ class GA4DiscoveryTest(DiscoveryTest, GA4Base):
     def streams_to_test(self):
         return self.expected_stream_names()
 
-    @unittest.skip("Does Not Apply")
     def test_stream_naming(self):
+        def naming_streams_to_test():
+            return self.expected_stream_names().difference({
+                'Test Report 1',
+                'Test Report 2',
+                'ecommerce_purchases_item_category_2_report',
+                'ecommerce_purchases_item_category_3_report',
+                'ecommerce_purchases_item_category_4_report',
+                'ecommerce_purchases_item_category_5_report'})
+        self.streams_to_test = naming_streams_to_test
+        super().test_stream_naming()
         """
         This tap accepts user provided stream names in the config via the report_definitions field
         and does not conform to the expectation that a stream's name must satisfy the condition:
