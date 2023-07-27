84112b4c1df362ed0712b6a9abc0f11c936d67fd
btowles <btowles@stitchdata.com>
2023-07-27 04:49:53 +0000

    Review comment updates round 1

--- tests/test_interrupted_sync.py
+++ tests/test_interrupted_sync.py
@@ -1,8 +1,7 @@
 import os
 from datetime import datetime as dt, timedelta
 
-import base
-from base import GA4Base
+from base import GA4Base, get_jira_card_status
 from tap_tester.base_suite_tests.interrupted_sync_test import InterruptedSyncTest
 
 
@@ -33,7 +32,6 @@ class GA4InterruptedSyncTest(InterruptedSyncTest, GA4Base):
         }
 
     def streams_to_selected_fields(self):
-        # TODO - when selecting these fields I'm getting errors.
         return {
             'content_group_report': self.expected_automatic_fields()['content_group_report'],
             'demographic_country_report': self.expected_automatic_fields()['demographic_country_report'],
@@ -73,12 +71,12 @@ class GA4InterruptedSyncTest(InterruptedSyncTest, GA4Base):
         # BUG override bookmark to use final state vs manipulate_state allowing test to pass
         done_status_list = [ "Closed", "Done", "Rejected" ]
         if not GA4InterruptedSyncTest.jira_status:
-            GA4InterruptedSyncTest.jira_status = base.get_jira_card_status('TDL-23687')
+            GA4InterruptedSyncTest.jira_status = get_jira_card_status('TDL-23687')
         self.assertNotIn(GA4InterruptedSyncTest.jira_status, done_status_list,
                          msg="JIRA BUG has transitioned to Done, remove work around")
         bookmark  = self.get_bookmark_value(self.resuming_sync_state, stream)
 
-        # The look back window should be used for completed streams
+        # The lookback window should be used for completed streams
         lookback = self.expected_lookback_window()
         if type(lookback) is dict:
             # lookback was retrieved from base_case or override all streams (dict)
@@ -97,7 +95,7 @@ class GA4InterruptedSyncTest(InterruptedSyncTest, GA4Base):
             return self.parse_date(bookmark) - stream_lookback
 
         # Expect the sync to start where the last sync left off,
-        #   expect to go back a look back for completed streams
+        #   expect to go back a lookback for completed streams
         #   but don't go back before the start date.
         if self.expected_start_date_behavior(stream):
             return max(
